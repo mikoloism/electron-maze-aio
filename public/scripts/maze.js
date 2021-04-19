4ce2d54a17eb39f49e89455f79e716e1a31eccb7
miko-github <mikoloism.github@gmail.com>
2021-04-20 03:38:19 +0430

    Update including system

--- public/scripts/maze.js
+++ public/scripts/maze.js
@@ -1,10 +1,1 @@
-const [generator, renderer, runner, solver] = [
-	require('./maze-generator'),
-	null,
-	null,
-	null,
-];
-(async () => {
-	const maze = generator();
-	console.log(maze);
-})();
+generator().then((res) => console.log(res));
