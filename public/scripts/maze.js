6482b289e1a1632a636dd8781de7e48f0fa80f09
miko-github <mikoloism.github@gmail.com>
2021-04-20 04:01:54 +0430

    Try-1.0 (🤔)

--- public/scripts/maze.js
+++ public/scripts/maze.js
@@ -1,1 +1,6 @@
-generator().then((res) => console.log(res));
+(async function () {
+	const maze = await generator(5, 5);
+	console.log(maze);
+	await render(maze);
+	await mount(document.getElementById('app'));
+})();
